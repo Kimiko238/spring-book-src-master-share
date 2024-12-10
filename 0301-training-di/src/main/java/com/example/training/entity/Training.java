95c1ae971dc9c7ed10ad33f40e2cfe3277329e9f
Kimiko238 <porigone58@yahoo.co.jp>
2024-12-10 17:19:20 +0900

    初回コミット

---
+++ 0301-training-di/src/main/java/com/example/training/entity/Training.java
@@ -0,0 +1,12 @@
+package com.example.training.entity;
+
+public class Training {
+    String s;
+    public String getTitle() {
+        return s;
+    }
+
+    public void setTitle(String s) {
+        this.s = s;
+    }
+}
